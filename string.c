26f99efd94804374d9fb88db45eaad20f3084452
jakub <jaduse@gmail.com>
2013-11-30 14:27:42 +0100

    StrSplit(char*, char, int*); function added.

--- string.c
+++ string.c
@@ -10,10 +10,11 @@
 string NewString();
 int StrSetdata(string *data, char *new_data);
 int StrLen(char *data);
-int StrIndexOf(char *data, char rexp);
+int StrIndexOf(char *data, char rexp, int start);
 int StrLastIndexOf(char *data, char rexp);
 int StrCompare(char *first, char *second);
 char* StrSubstring(char *data, int start, int end);
+char** StrSplit(char *data, char rexp, int *out_size);
 void StrReplace(char *data, char old_rexp, char new_rexp);
 void StrFree(string *data);
 
@@ -26,8 +27,8 @@ string NewString() {
 }
 
 int StrSetdata(string *data, char *str) {
-	size_t size = StrLen(str);
-	if((data->data = (char *)malloc(size+1)) == NULL)
+    size_t size = StrLen(str) + 1;
+    if((data->data = (char *)malloc(size)) == NULL)
 		return 0;
 	snprintf(data->data, size, str);
 	data->length = size;
@@ -38,14 +39,14 @@ int StrLen(char *data) {
 	int len;
 	for(len = 0; *(data + len) != '\0'; len++);	    
 	return len;        
-        
+
 }
 
 
-int StrIndexOf(char *data, char rexp) {
-	int i = 0;
+int StrIndexOf(char *data, char rexp, int start) {
+    int i = start;
 	char *c;
-	for(c = data; *c != rexp && *c; c++, i++);	
+    for(c = data; *c != rexp && *c; c++, i++);
 	if(i < StrLen(data))
 		return i;
 	
@@ -85,7 +86,7 @@ char* StrSubstring(char *data, int start, int end) {
 	char *new;
 
 	if((new = (char *)malloc(size + 1)) == NULL)
-		return NULL;
+        return NULL;
 	if(size > 0) {	
 		memcpy(new, &data[start], size);
 	}
@@ -95,6 +96,37 @@ char* StrSubstring(char *data, int start, int end) {
 	
 	return new;
 		
+}
+
+char **StrSplit(char *data, char rexp, int *out_size) {
+    char **p_new;
+    char *cpy, *start;
+    int i;
+    size_t size = StrLen(data);
+
+    for(cpy = data, i=1; *cpy; cpy++)
+        if(*cpy == rexp)
+            i++;
+
+    p_new = (char *) malloc(i * sizeof(char*));
+    memcpy((start = cpy = (char *)(p_new + i)), data, size);
+
+    while(*cpy) {
+        if(*cpy == rexp) {
+            *cpy++ = '\0';
+            *p_new++ = start;
+            start = cpy;
+
+
+        }
+        else
+            cpy++;
+
+    }
+    *p_new++ = start;
+    *out_size = i;
+    return p_new - i;
+
 }
 
 void StrReplace(char *data, char old_rexp, char new_rexp) {        
--- string.h
+++ string.h
@@ -9,9 +9,10 @@ typedef struct STRINGS {
 string NewString();
 int StrSetdata(string *data, char *str);
 int StrLen(char *data);
-int StrIndexOf(char *data, char rexp);
+int StrIndexOf(char *data, char rexp, int start);
 int StrLastIndexOf(char *data, char rexp);
 char* StrSubstring(char *data, int start, int end);
+char** StrSplit(char *data, char rexp, int *out_size);
 void StrReplace(char *data, char old_rexp, char new_rexp);
 void StrFree(string *data);
 int StrCompare(char *first, char *second);
